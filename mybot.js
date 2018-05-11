05bcfd0c1e38533cd876a7937572c528e0c3b430
Tim Neale <38301800+nealetw@users.noreply.github.com>
2018-05-11 12:04:13 -0400

    Add files via upload

--- mybot.js
+++ mybot.js
@@ -1,7 +1,14 @@
 const Discord = require("discord.js");
+const YTDL = require("ytdl-core");
+
+//const dispatcher = connection.playFile('C:/Users/Discord/Desktop/myfile.mp3');
+
+const commands = ["ping", "same", "help","join","leave", "d6", "d20", "play","stop"];
+
 const client = new Discord.Client();
 
-const commands = ["help", "same", "ping"];
+var servers = {};
+
 
 client.on("ready", () => {
   console.log("I am ready!");
@@ -9,12 +16,11 @@ client.on("ready", () => {
 });
 
 client.on("message", (message) => {
-  message.content
   if (message.content.startsWith('!')) {
+    var msg = message.content;
     var args = message.content.substring(1).split(' ');
-    var cmd = args[0];
+    var cmd = args[0].toLowerCase();
 
-    args = args.splice(1);
     switch(cmd) {
             // !ping
             case 'ping':
@@ -24,18 +30,87 @@ client.on("message", (message) => {
               message.channel.send('http://i0.kym-cdn.com/entries/icons/original/000/024/527/Screen_Shot_2017-10-31_at_12.17.45_PM.png');
               break;
             case 'help':
-              message.channel.send('You can send me any of these commands');
-              var mes;
+              var mes = 'You can send me any of these commands\n';
               for (var i = 0; i < commands.length; i++) {
                 mes = mes + commands[i];
                 mes = mes + "\n";
               }
-              message.channel.send(mes.substring(9));
+              message.channel.send(mes.substring(0));
               break;
             case 'kill':
               message.channel.send('Sorry, but i cant do that');
               break;
-  }
-}});
+            case 'join':
+                // Only try to join the sender's voice channel if they are in one themselves
+                if(!message.member.voiceChannel){
+                message.channel.sendMessage('Join a voice channel first, dummy');
+                return;
+              }
+                  message.member.voiceChannel.join()
+                    .then(connection => { // Connection is an instance of VoiceConnection
 
-client.login("");
+                })
+                  .catch(console.log);
+                  break;
+            case 'leave':
+              if(message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
+              break;
+            case 'd6':
+              var roll = Math.floor(Math.random() * 6+1);
+              message.reply('You rolled a ' + roll);
+              break;
+            case 'd20':
+              var roll = Math.floor(Math.random() * 20+1);
+              message.reply('You rolled a ' + roll);
+              break;
+            case "play":
+              if(!cmd[1])
+              {
+                message.channel.sendMessage('Please gib link');
+                return;
+              }
+
+              if(!message.member.voiceChannel){
+                message.channel.sendMessage('Join a voice channel first, dummy');
+                return;
+              }
+              if(!servers[message.guild.id]) servers[message.guild.id] = {
+                queue: []
+              };
+
+              var server = servers[message.guild.id];
+              server.queue.push(cmd.substring(4, cmd.length));
+
+              if(!message.guild.VoiceConnection) message.member.voiceChannel.join().then(function(connection) {
+                message.channel.sendMessage('joined and got to play function');
+
+                var url = msg.substr(5);
+
+                server.dispatcher = connection.playStream(YTDL(url, {filter: "audioonly"}));
+              });
+              break;
+            case "stop":
+              var server = servers[message.guild.id];
+              if(message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
+              break;
+
+  }
+
+}
+if (message.content.startsWith('oof')) {
+  if(message.member.voiceChannel)
+  {
+  message.member.voiceChannel.join()
+    .then(connection => { // Connection is an instance of VoiceConnection
+      const dispatcher = connection.playFile('C:/Users/neale/Documents/DiscordBot/oof.mp3');
+    });
+
+  }
+
+}
+
+
+
+});
+
+client.login("NDM0MDM3OTcyNjc3OTUxNTI1.DclX6A.xFohWrnk_C8r3VkMczFVcbTpNr0");
